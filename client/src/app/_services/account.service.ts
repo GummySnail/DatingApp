17deac95e51ad6fe5bd8220da80f22861a8757e8
Vladislav <vlad.mikhalchuk.01@mail.ru>
2022-01-25 23:24:48 +0300

    End of section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -22,8 +22,7 @@ export class AccountService {
         const user = response;
         if(user)
         {
-          localStorage.setItem("user", JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.serCurrentUser(user);
         }
       })
     )
@@ -34,14 +33,14 @@ export class AccountService {
       map((user: User) => {
         if(user)
         {
-          localStorage.setItem("user", JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.serCurrentUser(user);
         }
       })
     )
   }
 
   serCurrentUser(user: User){
+    localStorage.setItem("user", JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
