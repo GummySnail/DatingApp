94e0759a7fa75d78672ee922df013db4e0f03d02
Vladislav <vlad.mikhalchuk.01@mail.ru>
2022-02-02 01:56:50 +0300

    End of section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -9,5 +9,27 @@ namespace API.Data
         {
         }
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(k => new { k.SourceUserId, k.LikedUserId }); //Задает свойства, которые составляют
+                                                                     //первичный ключ для этого типа сущности.
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser) //Настраивает связь, в которой этот тип сущности
+                                           //имеет ссылку, указывающую на один экземпляр другого типа в связи.
+                .WithMany(l => l.LikedUsers) //Настраивает это отношение "один ко многим".
+                .HasForeignKey(s => s.SourceUserId) // Задаёт внешний ключ
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
